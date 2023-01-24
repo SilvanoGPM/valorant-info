2f32f875cf34261ede5b1fe1298ffdd6a546aa8e
SkyG0D <silvanosilvino@hotmail.com>
2023-01-24 00:35:49 -0300

    test: add test to find agent by name use case

---
+++ src/core/app/use-cases/agent/find-agent-by-name-use-case.spec.ts
@@ -0,0 +1,28 @@
+import { AgentNotFoundError } from '$core/app/errors/agent-not-found';
+import { Agent } from '$core/domain/entities/agent';
+import { makeAgent } from '$core/test/factories/agent-factory';
+import { InMemoryAgentGateway } from '$core/test/gateways/in-memory-agent-gateway';
+
+import { FindAgentByNameUseCase } from './find-agent-by-name-use-case';
+
+describe('Find agent by name use case', () => {
+  it('should be able to find an agent by name', async () => {
+    const agentGateway = new InMemoryAgentGateway([makeAgent()]);
+
+    const findByName = new FindAgentByNameUseCase(agentGateway);
+
+    const { agent } = await findByName.execute(makeAgent().name);
+
+    expect(agent).toBeInstanceOf(Agent);
+  });
+
+  it('should not be able to find an agent when it does not exists', async () => {
+    const agentGateway = new InMemoryAgentGateway([makeAgent()]);
+
+    const findById = new FindAgentByNameUseCase(agentGateway);
+
+    expect(() => {
+      return findById.execute('some-name');
+    }).rejects.toThrow(AgentNotFoundError);
+  });
+});
