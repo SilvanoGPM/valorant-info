ca225fefe1e8059924eef0ee5e493b0ec6e35b3c
SkyG0D <silvanosilvino@hotmail.com>
2023-07-13 01:19:08 -0300

    feat: add getStaticPaths to agent page

--- src/pages/agent/[name].tsx
+++ src/pages/agent/[name].tsx
@@ -3,19 +3,29 @@ import { GetStaticPaths, GetStaticProps } from 'next';
 import { AgentTemplate, AgentTemplateProps } from '$templates/agent-template';
 
 import type { FindAgentByNameUseCase } from '$core/app/use-cases/agent/find-agent-by-name-use-case';
+import { GetAgentsUseCase } from '$core/app/use-cases/agent/get-agents-use-case';
 import { container, Registry } from '$core/infra/container-registry';
 import { Agent } from '$core/domain/entities/agent';
+import { formatToURL } from '$utils/format-to-url';
 
 const findAgentByName = container.get<FindAgentByNameUseCase>(
   Registry.FindAgentByName,
 );
+
+const getAgents = container.get<GetAgentsUseCase>(Registry.GetAgents);
 
 export default function AgentPage(props: AgentTemplateProps) {
   return <AgentTemplate {...props} />;
 }
 
 export const getStaticPaths: GetStaticPaths = async () => {
-  return { paths: [], fallback: 'blocking' };
+  const { agents } = await getAgents.execute();
+
+  const paths = agents.map(({ name }) => ({
+    params: { name: formatToURL(name) },
+  }));
+
+  return { paths, fallback: 'blocking' };
 };
 
 export const getStaticProps: GetStaticProps<AgentTemplateProps> = async ({
